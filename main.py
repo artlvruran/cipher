68d2f52e63973a6401835fa144855e91a87af45f
Кирилл Марин <marincyrill@yandex.ru>
2024-04-08 18:02:10 +0300

    struct changes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from src.caesar_crypter import *
 from src.constants import en_alphabet
 from src.image_steganograph import ImageSteganograph
-from src.audio_steganograph import AudioSteganograph
 
-steganograph = AudioSteganograph('onepunchman_-_Opening_1_71148983.wav', 'hidden.wav')
-#steganograph.encrypt('Hello')
-steganograph.input_path = 'hidden.wav'
+steganograph = ImageSteganograph('saitama.png', 'hidden.png')
+steganograph.encrypt('Hello')
+steganograph.input_path = 'hidden.png'
 print(steganograph.decrypt())
---
+++ src/text_file_decoder.py
@@ -0,0 +1,4 @@
+from src.decoder_tool import DecoderTool
+
+class TextFileDecoder(DecoderTool):
+    self.inp
